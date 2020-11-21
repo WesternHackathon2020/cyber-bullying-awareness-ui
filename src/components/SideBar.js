8c1db776b9b25fc53c3c7d7b8fc714dcd3808769
Will Hutchinson <willhutchinson2.0@gmail.com>
2020-11-21 08:34:51 -0500

    sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ListItem from '@material-ui/core/ListItem';
 import SettingsIcon from '@material-ui/icons/Settings'
 import AssessmentIcon from '@material-ui/icons/Assessment'
 
-const useStyles = makeStyles({
-    list: {
-        width: 50,
-    },
-    fullList: {
-        width: 'auto',
-    },
-});
+const drawerWidth = 240;
 
-export default function TemporaryDrawer() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-start',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginRight: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: 0,
+    },
+}));
+
+export default function PersistentDrawerLeft(props) {
+    const [isOpen,handleClose] = useState(props);
     const classes = useStyles();
+    useEffect(() => {
+        handleClose(props);
+    }, [props]);
 
+    console.log(isOpen)
+    console.log(useState(props).isOpen)
     const list = () => (
         <div
             className={clsx(classes.list)}
@@ -39,9 +84,7 @@ export default function TemporaryDrawer() {
 
     return (
         <div>
-            <Drawer anchor={"left"} open={true}>
-                {list("left")}
-            </Drawer>
+                {list()}
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import SideBar from "../components/SideBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from '@material-ui/icons/Menu'
+import clsx from "clsx";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import useTheme from "@material-ui/core/styles/useTheme";
 
-const Home = () => (
+class Home extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            sideBarOpen: false,
+        }
+    }
+
+
+    render(){
+        const {sideBarOpen} = this.state;
+
+        return(
             <div className="homepage">
-                HOME
-                <SideBar/>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                >
+                    <Toolbar>
+                        <Typography variant="h6" noWrap>
+                            Persistent drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <div className="sidebar">
+                    <SideBar/>
+                </div>
+                <div className="main-section">
+                    MAIN
+                </div>
             </div>
         );
+    }
+}
 
 export default Home;
